c36f7b81497a151ef275222705885adff79c4996
okamotonr <r.okamoton@gmail.com>
2020-09-18 14:56:56 +0900

    rename

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,14 @@ fn argparse(args: &[String]) -> Result<&str, &'static str> {
     }
 }
 
-fn file_parser(file_path: &str) -> Result<BufReader<File>, Error> {
+fn get_reader(file_path: &str) -> Result<BufReader<File>, Error> {
     let file = File::open(file_path)?;
     Ok(BufReader::new(file))
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let reader = file_parser(argparse(&args).unwrap()).unwrap();
+    let reader = get_reader(argparse(&args).unwrap()).unwrap();
     let hack_parser = parser::Parser::new();
     let lines = hack_parser.parse(reader).unwrap();
     let code_generator = code::CodeGenerator::new(lines);
